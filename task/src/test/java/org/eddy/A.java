86939cb4a9d6a57d2b4d83bcdb4a619d58b4569e
Justice-love <eddyxu1213@126.com>
2017-12-08 20:28:39 +0800

    脚本运行沙箱环境改造

---
+++ task/src/test/java/org/eddy/A.java
@@ -0,0 +1,8 @@
+package org.eddy;
+
+public class A {
+
+    public void a() {
+        System.out.println("a");
+    }
+}
--- task/src/test/java/org/eddy/EngineTest.java
+++ task/src/test/java/org/eddy/EngineTest.java
@@ -1,8 +1,12 @@
 package org.eddy;
 
+import net.sourceforge.prograde.policy.ProgradePolicyFile;
+import net.sourceforge.prograde.sm.ProgradeSecurityManager;
 import org.eddy.engine.Engine;
 import org.eddy.engine.GroovyEngine;
 import org.junit.Test;
+
+import java.security.Policy;
 
 public class EngineTest {
 
@@ -14,7 +18,20 @@ public class EngineTest {
 
     @Test
     public void test2() {
+        System.setProperty("java.security.policy", "/Users/xuyi/java/star-map/task/src/test/resources/java.policy");
+        System.setSecurityManager(new SecurityManager());
         Engine engine = new GroovyEngine();
         engine.execute("print('test')");
+    }
+
+    @Test
+    public void test3() {
+        System.setProperty("java.security.policy", "/Users/xuyi/java/star-map/task/src/test/resources/java.policy");
+//        System.setProperty("java.security.manager", "net.sourceforge.prograde.sm.ProgradeSecurityManager");
+//        Policy.setPolicy(new ProgradePolicyFile());
+        System.setSecurityManager(new ProgradeSecurityManager());
+        Engine engine = new GroovyEngine();
+        engine.execute("org.eddy.A a = new org.eddy.A()\n" +
+                "a.a()");
     }
 }
