0a9512b167f5d74916feb682351a96d4d2c6606c
Justice-love <eddyxu1213@126.com>
2017-12-11 10:35:31 +0800

    增加沙箱模块

---
+++ sandbox/src/main/java/org/eddy/permission/annotation/Permission.java
@@ -0,0 +1,9 @@
+package org.eddy.permission.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Permission {
+}
---
+++ sandbox/src/main/java/org/eddy/permission/aop/PermissionAspect.java
@@ -0,0 +1,9 @@
+package org.eddy.permission.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class PermissionAspect {
+}
