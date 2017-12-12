64c6aa6470c774b6d677dab0fcb1a8ced515aaf0
Justice-love <eddyxu1213@126.com>
2017-12-12 10:07:26 +0800

    增加zk测试

---
+++ registry/src/test/java/org/eddy/ZkTest.java
@@ -0,0 +1,28 @@
+package org.eddy;
+
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ZkTest {
+
+    @Test
+    public void test() throws IOException {
+        ZkClient zkc = new ZkClient(new ZkConnection("192.168.23.136:2181"), 3_000);
+//        zkc.createEphemeral("/star");
+//        zkc.createPersistent("/star");
+//        zkc.createPersistent("/star/provider");
+        zkc.subscribeChildChanges("/star/provider", new IZkChildListener() {
+            @Override
+            public void handleChildChange(String parentPath, List<String> currentChildren) throws Exception {
+                currentChildren.stream().forEach(s -> System.out.println(s));
+            }
+        });
+
+        System.in.read();
+    }
+}
