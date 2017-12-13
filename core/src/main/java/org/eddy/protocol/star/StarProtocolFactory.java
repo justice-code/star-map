7c311477198d1a7a8bfca1e705072cd449092fde
Justice-love <eddyxu1213@126.com>
2017-12-13 17:59:31 +0800

    注入server client

--- core/src/main/java/org/eddy/protocol/star/StarProtocolFactory.java
+++ core/src/main/java/org/eddy/protocol/star/StarProtocolFactory.java
@@ -3,18 +3,28 @@ package org.eddy.protocol.star;
 import org.eddy.protocol.ClientProtocol;
 import org.eddy.protocol.ProtocolFactory;
 import org.eddy.protocol.ServerProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("StarProtocol")
 public class StarProtocolFactory implements ProtocolFactory {
 
+    @Autowired
+    @Qualifier("starClient")
+    private ClientProtocol clientProtocol;
+
+    @Autowired
+    @Qualifier("starServer")
+    private ServerProtocol serverProtocol;
+
     @Override
     public ClientProtocol client() {
-        return null;
+        return clientProtocol;
     }
 
     @Override
     public ServerProtocol server() {
-        return null;
+        return serverProtocol;
     }
 }
