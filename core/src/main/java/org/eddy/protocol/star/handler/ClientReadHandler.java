301287162366ca8c83bdb934ee0d277b595e3c0d
Justice-love <eddyxu1213@126.com>
2017-12-14 14:01:17 +0800

    处理长链接通信粘包问题

--- core/src/main/java/org/eddy/protocol/star/handler/ClientReadHandler.java
+++ core/src/main/java/org/eddy/protocol/star/handler/ClientReadHandler.java
@@ -2,10 +2,12 @@ package org.eddy.protocol.star.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.eddy.protocol.Data;
 
-public class ClientReadHandler extends SimpleChannelInboundHandler {
+public class ClientReadHandler extends SimpleChannelInboundHandler<Data> {
+
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, Data msg) throws Exception {
+        System.out.println(msg);
     }
 }
