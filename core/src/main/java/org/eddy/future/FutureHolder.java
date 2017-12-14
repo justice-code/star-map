fa2175e1d8b065703d3d3df5e3645c79747b5891
Justice-love <eddyxu1213@126.com>
2017-12-14 15:47:15 +0800

    使用分段字典

--- core/src/main/java/org/eddy/future/FutureHolder.java
+++ core/src/main/java/org/eddy/future/FutureHolder.java
@@ -2,13 +2,13 @@ package org.eddy.future;
 
 import org.eddy.protocol.Data;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class FutureHolder {
 
-    private static Map<String, StarFuture> holder = new HashMap<>();
+    private static Map<String, StarFuture> holder = new ConcurrentHashMap<>();
 
     public static void receive(Data result) {
         Optional.ofNullable(holder.remove(result.getId())).orElseThrow(() -> new RuntimeException("can not find future")).receive(result);
